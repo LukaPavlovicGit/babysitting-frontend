9f06f659c296605dc13e193c04db3ae257668a83
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-12-07 23:12:16 +0100

    added css autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
+import autoprefixer from "autoprefixer";
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {}, // SCSS opcije
+    },
+    postcss: {
+      plugins: [autoprefixer()], // PostCSS pluginovi
+    },
+  },
+  plugins: [react() as PluginOption],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
 })
