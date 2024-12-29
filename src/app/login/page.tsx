b9cbee7ff2883061ad987f71a39f77b448d46c9b
LukaPavlovicGit <lukapavlovic032@gmail.com>
2024-12-29 01:54:27 +0100

    working on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,31 @@
+import Link from "next/link";
+import {LeftArrowSvg} from "../../assets/svg/left-arrow";
+
 function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center justify-center border-[1px] border-white p-4 w-full sm:w-1/2 gap-6">
-        <h1 className="text-2xl font-bold">Welcome</h1>
-        <div className="flex flex-col size-full gap-3">
-          <p className="text-sm text-gray-500">Log in or sign up</p>
-          <input type="text" placeholder="Email" className="px-2 py-1 text-black" />
-          <button className="bg-yellow-500 text-black px-2 py-1">Continue</button>
+        <div className="flex flex-col items-center justify-center border-[1px] border-white p-4 w-full sm:w-1/2 sm:max-w-md gap-6">
+            <div className="relative flex flex-row items-center justify-between gap-3 w-full">
+                {/* I want to link this button to the login-or-signup page */}
+                <Link href="/login-or-signup" className="bg-white text-black p-2">
+                    <LeftArrowSvg/>
+                </Link>
+                <h2 className="absolute text-2xl font-bold left-1/2 -translate-x-1/2">Welcome, John</h2>
+            </div>
+            <div className="w-full text-white text-center">{`tteeeeeesssssssst@gmail.com`}</div>
+            <form className="flex flex-col items-center justify-center gap-3 w-full">
+                <input type="password" placeholder="Password" className="w-full p-2 text-black"/>
+                <button className="bg-green-500 text-black px-2 py-1 w-full">Login</button>
+            </form>
+            <Link href="/forgot-password" className="text-white text-left w-full underline">Forgot password?</Link>
+            <div className="flex items-center justify-center w-full gap-3">
+                <button className="bg-white text-black p-2">Apple</button>
+                <button className="bg-white text-black p-2">Facebook</button>
+                <button className="bg-white text-black p-2">Google</button>
+            </div>
         </div>
-        {/* i want horizontal line between the two sections.. but it should be 1px thick and white, and it should be 100% width, and it should hold text "or continue with" in the middle.. so tehre is left part of the hor line, then the text and then right part of the hor line */}
-        <div className="flex items-center justify-center w-full">
-          <p className="text-sm text-gray-500 ">or continue with</p>
-        </div>
-        {/* I want to match three buttons in the middle of the page  */}
-        <div className="flex items-center justify-center w-full gap-3">
-          <button className="bg-white text-black p-2">Apple</button>
-          <button className="bg-white text-black p-2">Facebook</button>
-          <button className="bg-white text-black p-2">Google</button>
-        </div>
-      </div>
     </div>
-  );
+  )
 }
 
 export default LoginPage;
