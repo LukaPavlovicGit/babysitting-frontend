b164149aaa77ee22dff81d79279ed5f296481e5c
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-02 19:43:13 +0100

    useGetAccountByEmail fix according to the prev changes

--- src/hooks/api/useGetAccountByEmail.ts
+++ src/hooks/api/useGetAccountByEmail.ts
@@ -1,33 +1,34 @@
+'use client'
+
 import { useRouter } from 'next/navigation'; // Use next/navigation
 import { useMutation } from '@tanstack/react-query';
 import { api } from '@/config/api';
 
 
 interface GetAccountByEmailResponse {
-    firstName: string;
-    email: string;
-  }
+  firstName: string;
+  email: string;
+  userId: string;
+  lastName: string;
+}
   
-  export const useGetAccountByEmail = () => {
+export const useGetAccountByEmail = () => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    return useMutation({
-      mutationFn: async (email: string) => {
-        const res = await fetch(`${api.endpoints.account.getByEmail(email)}`, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email })
-        });
-        
-        if (!res.ok) throw new Error('Failed to check email');
-        return res.json() as Promise<GetAccountByEmailResponse>;
-      },
-      onSuccess: (data) => {
-        router.replace('/');
-      },
-      onError: (error) => {
-        
-      }
-    });
-  };
+  return useMutation({
+    mutationFn: async (email: string) => {
+      const res = await api.endpoints.account.getByEmail(email);
+      console.log(res)
+      if (!res.ok) throw new Error('Failed to check email');
+      const data = await res.json();
+      return data as GetAccountByEmailResponse;
+    },
+    onSuccess: (data) => {
+      console.log("data: " + data.firstName)
+    },
+    onError: (error) => {
+      console.log("error: " + error)
+    }
+  });
+};
