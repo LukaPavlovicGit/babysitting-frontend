25df20829143f5e1dc59905c83652b3db7d52023
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-07 02:29:31 +0100

    login page with prepolate logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
 import Link from "next/link";
-import {LeftArrowSvg} from "../../assets/svg/left-arrow";
+import { LeftArrowSvg } from "../../assets/svg/left-arrow";
+import { useForm } from 'react-hook-form';
+import { LoginData, loginSchema } from '@/schemas/loginSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import FormField from '@/components/FormField';
+import { useAccountLogin } from '@/hooks/api/useAccountLogin';
 
 function LoginPage() {
+  const searchParams = useSearchParams();
+  const email = searchParams.get('email');
+  const firstName = searchParams.get('firstName');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors } 
+  } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  const { mutate } = useAccountLogin();
+
+  const onSubmit = handleSubmit((data) => {
+    mutate(data)
+  })
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
         <div className="flex flex-col items-center justify-center border-[1px] border-white p-4 w-full sm:w-1/2 sm:max-w-md gap-6">
@@ -10,11 +36,26 @@ function LoginPage() {
                 <Link href="/login-or-signup" className="bg-white text-black p-2">
                     <LeftArrowSvg/>
                 </Link>
-                <h2 className="absolute text-2xl font-bold left-1/2 -translate-x-1/2">Welcome, John</h2>
+                <h2 className="absolute text-2xl font-bold left-1/2 -translate-x-1/2">Welcome, {firstName}</h2>
             </div>
-            <div className="w-full text-white text-center">{`tteeeeeesssssssst@gmail.com`}</div>
-            <form className="flex flex-col items-center justify-center gap-3 w-full">
-                <input type="password" placeholder="Password" className="w-full p-2 text-black"/>
+            <form onSubmit={onSubmit} className="flex flex-col items-center justify-center gap-3 w-full">
+                <FormField
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    register={register}
+                    error={errors.email}
+                    className="w-full px-2 py-1 text-black "
+                    value={email || ''}
+                />
+                <FormField
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    register={register}
+                    error={errors.password}
+                    className="w-full px-2 py-1 text-black "
+                />
                 <button className="bg-green-500 text-black px-2 py-1 w-full">Login</button>
             </form>
             <Link href="/forgot-password" className="text-white text-left w-full underline">Forgot password?</Link>
