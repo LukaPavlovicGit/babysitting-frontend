628da8d9378c417e1fe47e59019d67f63c0808dc
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-07 02:28:20 +0100

    useGetAccountByEmai updates

--- src/hooks/api/useGetAccountByEmail.ts
+++ src/hooks/api/useGetAccountByEmail.ts
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/navigation'; // Use next/navigation
+import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { api } from '@/config/api';
 
@@ -17,16 +17,18 @@ export const useGetAccountByEmail = () => {
   return useMutation({
     mutationFn: async (email: string) => {
       const res = await api.endpoints.account.getByEmail(email);
-      console.log(res)
-      if (!res.ok) throw new Error('Failed to check email');
+
+      if (!res.ok)
+        throw new Error('Failed to check email');
+      
       const data = await res.json();
       return data as GetAccountByEmailResponse;
     },
     onSuccess: (data) => {
-      router.replace('/login');
+      router.push(`/login?email=${data.email}&firstName=${data.firstName}`);
     },
     onError: (error) => {
-      router.replace('/signup');
+      router.push('/signup');
     }
   });
 };
