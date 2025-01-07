ce1be6f35ecff54d6ad8be56cfbbf7b869b61cc0
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-07 02:26:13 +0100

    created login schema

---
+++ src/schemas/loginSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { emailPattern, passwordPattern } from './shared/patterns';
+
+export const loginSchema = z.object({
+  email: emailPattern,
+  password: passwordPattern,
+  rememberMe: z.boolean().optional(),
+});
+
+export type LoginData = z.infer<typeof loginSchema>;
