2060d0e2de8753607b712aaef833a878a4cd90b2
LukaPavlovicGit <lukapavlovic032@gmail.com>
2025-01-17 00:14:21 +0100

    endpoits updates

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,18 +1,18 @@
 export const ENDPOINTS = {
-    account: {
-      login: '/account/login',
-      register: '/account/register',
-      logout: '/account/logout',
-      resetPassword: '/account/reset-password',
-      get: '/account',
-      getByEmail: (email: string) => `/account/get-account-by-email/${email}`,
-      update: '/account/update',
-      settings: '/account/settings',
-    },
-    bookings: {
-      list: '/bookings',
-      create: '/bookings/create',
-      cancel: (id: string) => `/bookings/${id}/cancel`,
-      details: (id: string) => `/bookings/${id}`,
-    },
-  } as const;
+  account: {
+    login: '/account/login',
+    signup: '/account/register',
+    logout: '/account/logout',
+    resetPassword: '/account/reset-password',
+    get: '/account',
+    getByEmail: (email: string) => `/account/get-account-by-email/${email}`,
+    update: '/account/update',
+    settings: '/account/settings',
+  },
+  bookings: {
+    list: '/bookings',
+    create: '/bookings/create',
+    cancel: (id: string) => `/bookings/${id}/cancel`,
+    details: (id: string) => `/bookings/${id}`,
+  },
+} as const
